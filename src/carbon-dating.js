ac0377ce151e4aa4c9dca522eba8c057304d9d2f
Maxim Kalevich <maxkalevich@gmail.com>
2021-03-04 16:11:13 +0300

    fix: fix bag

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,13 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-    if (typeof sampleActivity !== "string" || isNaN(sampleActivity)) return false;
-    if ( !Number(sampleActivity[0]) ) return false;
-    if (sampleActivity >= 15) return false;
-  let k = (Math.log(2))/HALF_LIFE_PERIOD;
-  let t = (Math.log(MODERN_ACTIVITY/sampleActivity))/k;
-  return Math.ceil(Number(t));
+    if (sampleActivity >= 15 || typeof sampleActivity !== "string" || isNaN(sampleActivity) || !Number(sampleActivity[0])) return false;
+    return Math.ceil(Number((Math.log(MODERN_ACTIVITY / sampleActivity)) / ((Math.log(2)) / HALF_LIFE_PERIOD)));
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  let arr = matrix.flat();
+  const arr = [].concat(...matrix);
   let count = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "^^") {
