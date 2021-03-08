5ca12ab2401e8065bb607954a3e7397899760101
Maxim Kalevich <maxkalevich@gmail.com>
2021-03-08 21:46:01 +0300

    fix: complete task

--- src/transform-array.js
+++ src/transform-array.js
@@ -4,30 +4,54 @@ module.exports = function transform(arr) {
   if(!Array.isArray(arr)) {
     throw new Error('Not implemented');
   }
+  const newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(arr[i])
+  }
 
-  const newArray = [].concat(...arr);
-  newArray.map((el, index, arr) => {
-    if (index === 0 && index === arr.length - 1 ) {
-      return arr
-    }
-    if (el === "--discard-prev" && typeof el !== undefined) {
+  const discardNext = "--discard-next"; // исключает следующий элемент массива из преобразованного массива
+  const discardPrev = "--discard-prev"; // исключает предыдущий элемент массива из преобразованного массива
+  const doubleNext = "--double-next"; // удваивает следующий элемент массива в преобразованном массиве
+  const doublePrev = "--double-prev"; // удваивает предыдущий элемент массива в преобразованном массиве
+
+
+
+  newArr.map((el, index, arr) => {
+
+    if (el === "--discard-prev" && index === 0) {
+      arr.splice(index, 1, "")
+
+    } else if (el === "--discard-prev") {
       arr.splice(index, 1);
-      arr.splice(index-1, 1);
+      arr.splice(index - 1, 1, "");
     }
-    if (el === "--discard-next" && typeof el !== undefined) {
+
+    if (el === "--discard-next" && index === arr[arr.length - 1]) {
+      arr.splice(index, 1, "")
+
+    } else if (el === "--discard-next") {
       arr.splice(index, 1);
-      arr.splice(index+1, 1);
+      arr.splice(index, 1, "");
     }
-    if (el === "--double-next" && typeof el !== undefined) {
-      arr.splice(index, 1); // Удаляем "--double-next"
-      arr.splice(index+1, 0, arr[index]); // Вставляем элемент , который встал на место "--double-next"
+    if (el === "--double-next" && index === arr[arr.length - 1]) {
+      arr.splice(index, 1, "")
+
+    } else if (el === "--double-next") {
+      arr.splice(index, 1);
+      arr.splice(index + 1, 0, arr[index]);
     }
-    if (el === "--double-prev" && typeof el !== undefined) {
-      arr.splice(index, 1); // Удаляем "--double-prev"
-      arr.splice(index, 0, arr[index-1]); // Вставляем элемент , который встал на место "--double-next"
+    if (el === "--double-prev" && index === 0) {
+      arr.splice(index, 1, "")
+
+    } else if (el === "--double-prev") {
+      arr.splice(index, 1);
+      arr.splice(index-1, 0, arr[index-1])
     }
 
   })
-  return newArray
 
+  const res = newArr.
+  filter(el => el !== "" && el !== undefined)
+
+  return res
 };
