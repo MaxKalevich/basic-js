d6a973d4e38825d087dd56526e89ba4e321fc6fc
Maxim Kalevich <maxkalevich@gmail.com>
2021-03-08 19:47:08 +0300

    fix: fix bag

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,4 +5,29 @@ module.exports = function transform(arr) {
     throw new Error('Not implemented');
   }
 
+  const newArray = [].concat(...arr);
+  newArray.map((el, index, arr) => {
+    if (index === 0 && index === arr.length - 1 ) {
+      return arr
+    }
+    if (el === "--discard-prev" && typeof el !== undefined) {
+      arr.splice(index, 1);
+      arr.splice(index-1, 1);
+    }
+    if (el === "--discard-next" && typeof el !== undefined) {
+      arr.splice(index, 1);
+      arr.splice(index+1, 1);
+    }
+    if (el === "--double-next" && typeof el !== undefined) {
+      arr.splice(index, 1); // Удаляем "--double-next"
+      arr.splice(index+1, 0, arr[index]); // Вставляем элемент , который встал на место "--double-next"
+    }
+    if (el === "--double-prev" && typeof el !== undefined) {
+      arr.splice(index, 1); // Удаляем "--double-prev"
+      arr.splice(index, 0, arr[index-1]); // Вставляем элемент , который встал на место "--double-next"
+    }
+
+  })
+  return newArray
+
 };
